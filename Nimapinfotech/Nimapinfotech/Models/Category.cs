8b658a8fe45988871e609e8693c8233b4debe764
Harshal Mutyalwar <harshalmutyalwar@gmail.com>
2024-12-07 14:41:52 +0530

    Model Class Added

---
+++ Nimapinfotech/Nimapinfotech/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nimapinfotech.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column("CategoryName", TypeName = "varchar(100)")]
+        [Required]
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ Nimapinfotech/Nimapinfotech/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nimapinfotech.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Column("ProductName", TypeName = "varchar(100)")]
+        [Required]
+        public string ProductName { get; set; }
+
+       
+        public int CategoryId { get; set; }
+    }
+}
